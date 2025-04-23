8b3b092f069292b1a507a29aaf65b5748a8e17b9
Duong-Phuc-Thinh <thinhtattoo2006@gmail.com>
2025-04-22 22:23:40 -0700

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,16 @@
+const loginForm = document.querySelector(".form-box.login");
+const registerForm = document.querySelector(".form-box.register");
+const logregBox = document.querySelector(".logreg-box");
+
+// Button để chuyển sang form đăng ký
+const registerLink = document.querySelector(".register-link");
+registerLink.addEventListener("click", () => {
+    logregBox.classList.add("active");
+});
+
+// Button để quay lại form đăng nhập
+const loginLink = document.querySelector(".login-link");
+loginLink.addEventListener("click", () => {
+    logregBox.classList.remove("active");
+});
+
